a9376ca9561ff603aa2e369d6eee054033d90b0f
Igor Pieper <91740800+IgorPieper@users.noreply.github.com>
2022-11-16 19:25:15 +0100

    Create ObjectTwo.java

---
+++ Materials/Listy_Generyczne/ObjectTwo.java
@@ -0,0 +1,11 @@
+package klasy;
+
+public class ObjectTwo {
+    int sila;
+    int wytrzymalosc;
+
+    public ObjectTwo(int sila, int wytrzymalosc) {
+        this.sila = sila;
+        this.wytrzymalosc = wytrzymalosc;
+    }
+}
