75132f6eee52116b26b9a63a259a4757e4f49d97
Igor Pieper <91740800+IgorPieper@users.noreply.github.com>
2022-11-16 19:29:44 +0100

    Create Main.java

---
+++ Materials/Klasy_Generyczne/Main.java
@@ -0,0 +1,11 @@
+import klasy.GenericClass;
+
+public class Main {
+    public static void main(String[] args) {
+
+            GenericClass<String> napis = new GenericClass<>();
+            napis.setRzecz("Super Napis");
+            System.out.println(napis.getRzecz());
+
+    }
+}
