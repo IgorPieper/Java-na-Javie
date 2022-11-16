6b667f8845894ab324c45f1ca0fc28feb1f15b9c
Igor Pieper <91740800+IgorPieper@users.noreply.github.com>
2022-11-16 19:24:58 +0100

    Create ObjectOne.java

---
+++ Materials/Listy_Generyczne/ObjectOne.java
@@ -0,0 +1,12 @@
+package klasy;
+
+public class ObjectOne {
+    public String imie;
+    String nazwisko;
+
+    public ObjectOne(String imie, String nazwisko) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+    }
+
+}
