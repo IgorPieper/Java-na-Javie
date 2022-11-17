a79111be86afffcc4d4970822611e2cd2f957dde
Igor Pieper <91740800+IgorPieper@users.noreply.github.com>
2022-11-17 12:07:51 +0100

    Create Main.java

---
+++ Materials/Adnotacje/Podstawowa_Adnotacja/Main.java
@@ -0,0 +1,13 @@
+import klasy.Adnotacja;
+import klasy.Funkcyja;
+import klasy.GenericClass;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        @Adnotacja()
+        int procent;
+    }
+}
