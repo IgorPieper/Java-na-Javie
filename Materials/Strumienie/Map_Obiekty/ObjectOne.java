9f25fd29fb2986340dccf202a329538f0df03cdc
Igor Pieper <91740800+IgorPieper@users.noreply.github.com>
2022-11-17 19:21:59 +0100

    Create ObjectOne.java

---
+++ Materials/Strumienie/Map_Obiekty/ObjectOne.java
@@ -0,0 +1,13 @@
+package klasy;
+
+public class ObjectOne {
+    private String imie;
+
+    public ObjectOne(String imie) {
+        this.imie = imie;
+    }
+
+    public String getImie() {
+        return imie;
+    }
+}
