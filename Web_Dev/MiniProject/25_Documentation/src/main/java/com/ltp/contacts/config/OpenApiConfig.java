cde63ea74ccb522dcc2ed57264a27227cecd5773
Igor Pieper <91740800+IgorPieper@users.noreply.github.com>
2023-01-04 13:11:45 +0100

    Add files via upload

---
+++ Web_Dev/MiniProject/25_Documentation/src/main/java/com/ltp/contacts/config/OpenApiConfig.java
@@ -0,0 +1,23 @@
+package com.ltp.contacts.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.*;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    OpenAPI openApi(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Contact API")
+                        .description("An API that can manage contacts")
+                        .version("v1.0"));
+    }
+}
+
+// http://localhost:8080/v3/api-docs
+// http://localhost:8080/swagger-ui/index.html
